84b2e5aa3ba344efb153bd967a8e2c6bf1c22df7
Billbern <denis.abaidoo@yahoo.com>
2020-12-05 16:05:50 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express'),
+         path = require('path');
+const { env } = require('process');
+
+
+let app = express();
+app.use(express.static(path.join(__dirname, 'static')));
+app.set("view engine", "ejs");
+
+
+
+app.get('/', (req, res)=>{
+    res.render('home');
+})
+
+const port = process.env.PORT || 5050;
+
+app.listen(port, "127.0.0.1", ()=>{
+    console.log(`server is starting......\nserver running on http://127.0.0.1:${port}`);
+});
